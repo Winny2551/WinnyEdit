612acab63a2613a48b3a4dc73278af31bf64cc06
GitHubxWinny <131293878+Winny2551@users.noreply.github.com>
2023-05-18 12:54:50 +0700

    Update Edit.lua

--- Edit.lua
+++ Edit.lua
@@ -1,6 +1,6 @@
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Winny Hub", "LightTheme")
+local Window = Library.CreateLib("Winny Hub", "BloodTheme")
 
 local Tab = Window:NewTab("Credit")
 local Section = Tab:NewSection("Winny x Edit")
