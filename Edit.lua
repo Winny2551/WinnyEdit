8262bc8bf77bdd0d48fa32fbfbad2d5d487552ae
GitHubxWinny <131293878+Winny2551@users.noreply.github.com>
2023-05-18 10:16:39 +0700

    Update Edit.lua

--- Edit.lua
+++ Edit.lua
@@ -1,1 +1,9 @@
 
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Winny Hub", "LightTheme")
+
+local Tab = Window:NewTab("Credit")
+local Section = Tab:NewSection("Winny x Edit")
+Section:NewLabel("FB | Winny Exe")
+Section:NewButton("ButtonText", "ButtonInfo", function()
+setclipboard("")
