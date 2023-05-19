721be47a81ed65258983c91bf170e2871d791cfa
GitHubxWinny <131293878+Winny2551@users.noreply.github.com>
2023-05-19 13:35:34 +0700

    Update NotHaveKey.lua

--- NotHaveKey.lua
+++ NotHaveKey.lua
@@ -1,21 +1,5 @@
 
 
-function RobloxNotify(Ti,tab,icon,sec)
 
-    game.StarterGui:SetCore("SendNotification", {
 
-    Title = Ti,
-
-    Text = tab,
-
-    Icon = icon,
-
-    Duration = sec,
-
-    })
-
-end
-
-RobloxNotify("Test","what",nil,2)
-
-game.Players.LocalPlayer:Kick("Invalid Key! Please Rejoin And Try Again.")
+game.Players.LocalPlayer:Kick("มึงไม่มีคีย์ไอควายยังจะรันสคริป")
